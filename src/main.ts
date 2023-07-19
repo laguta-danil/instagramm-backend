0e9beacd58c6eb6d0c9084b63b6509f133b53746
laguta.d.s@gmail.com <laguta.d.s@gmail.com>
2023-07-19 20:01:39 +0300

    fix/swagger

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 const config = new DocumentBuilder()
-  .setTitle('Clouds data')
-  .setDescription('Clouds description')
+  .setTitle('Backend Generator')
+  .setDescription('Documentation API Test')
   .setVersion('1.0')
+  .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
   .build();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
