901baa7ab9aa3220baa5ebc70d4c8cf6b6f8e39f
Andrii Chertkov <andriichertkov@gmail.com>
2023-07-19 19:01:29 +0300

    add nest config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   controllers: [AppController],
-  imports: [],
+  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true })],
   providers: [AppService]
 })
 export class AppModule {}
