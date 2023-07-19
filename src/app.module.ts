9f57fdb20c3a545650f69cf3e619562a206ada7b
Andrii Chertkov <andriichertkov@gmail.com>
2023-07-19 19:31:54 +0300

    add mailer config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
+import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MailerConfig } from './config/mailer.config';
 
 @Module({
   controllers: [AppController],
-  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    MailerModule.forRootAsync({ useClass: MailerConfig })
+  ],
   providers: [AppService]
 })
 export class AppModule {}
