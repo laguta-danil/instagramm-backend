0e960de366ef05d713495c8b07b7ce0d22ca0150
laguta.d.s@gmail.com <laguta.d.s@gmail.com>
2023-07-19 21:03:27 +0300

    fix/swagger4

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,21 @@
 import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MailerConfig } from './config/mailer.config';
 import { EmailModule } from './modules/email/email.module';
+import { join } from 'path';
 
 @Module({
   controllers: [AppController],
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client')
+    }),
     MailerModule.forRootAsync({ useClass: MailerConfig }),
     EmailModule
   ],
