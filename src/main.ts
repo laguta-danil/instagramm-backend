c40b8779303dd089968d7a391ef03ed6f9d6d90a
laguta.d.s@gmail.com <laguta.d.s@gmail.com>
2023-07-19 22:01:55 +0300

    fix/swagger25

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { createWriteStream } from 'fs';
-import { get } from 'http';
+import { resolve } from 'path';
+import { writeFileSync } from 'fs';
 import * as process from 'process';
 
 import { AppModule } from './app.module';
@@ -23,48 +23,16 @@ async function bootstrap() {
 
   // get the swagger json file (if app is running in development mode)
   if (process.env.NODE_ENV === 'development') {
-    // write swagger ui files
-    get(
-      `${process.env.DEPLOYED_URL}/swagger/swagger-ui-bundle.js`,
-      function (response) {
-        response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
-        console.log(
-          `Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`
-        );
-      }
-    );
+    const pathToSwaggerStaticFolder = resolve(process.cwd(), 'swagger-static');
 
-    get(
-      `${process.env.DEPLOYED_URL}/swagger/swagger-ui-init.js`,
-      function (response) {
-        response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
-        console.log(
-          `Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`
-        );
-      }
+    // write swagger json file
+    const pathToSwaggerJson = resolve(
+      pathToSwaggerStaticFolder,
+      'swagger.json'
     );
-
-    get(
-      `${process.env.DEPLOYED_URL}/swagger/swagger-ui-standalone-preset.js`,
-      function (response) {
-        response.pipe(
-          createWriteStream('swagger-static/swagger-ui-standalone-preset.js')
-        );
-        console.log(
-          `Swagger UI standalone preset file written to: '/swagger-static/swagger-ui-standalone-preset.js'`
-        );
-      }
-    );
-
-    get(
-      `${process.env.DEPLOYED_URL}/swagger/swagger-ui.css`,
-      function (response) {
-        response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
-        console.log(
-          `Swagger UI css file written to: '/swagger-static/swagger-ui.css'`
-        );
-      }
-    );
+    const swaggerJson = JSON.stringify(document, null, 2);
+    writeFileSync(pathToSwaggerJson, swaggerJson);
+    console.log(`Swagger JSON file written to: '/swagger-static/swagger.json'`);
   }
 }
 bootstrap();
