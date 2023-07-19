1eebc94064532d70cf6cbaaf7ad0863450eab2a0
Andrii Chertkov <andriichertkov@gmail.com>
2023-07-19 19:35:28 +0300

    add email module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MailerConfig } from './config/mailer.config';
+import { EmailModule } from './modules/email/email.module';
 
 @Module({
   controllers: [AppController],
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
-    MailerModule.forRootAsync({ useClass: MailerConfig })
+    MailerModule.forRootAsync({ useClass: MailerConfig }),
+    EmailModule
   ],
   providers: [AppService]
 })
