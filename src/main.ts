7515534d544f058f1e579daba6bfbc4f6d8cecf6
laguta.d.s@gmail.com <laguta.d.s@gmail.com>
2023-07-19 21:48:47 +0300

    fix/swagger24

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
+  SwaggerModule.setup('/swagger', app, document);
 
   await app.listen(3000);
 
