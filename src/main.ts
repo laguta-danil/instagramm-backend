51655c7e043bb8a2ba8316f92bb896a4e8957cd2
laguta.d.s@gmail.com <laguta.d.s@gmail.com>
2023-07-19 20:44:17 +0300

    fix/swagger

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,9 @@ const config = new DocumentBuilder()
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
+  SwaggerModule.setup('swagger', app, document, {
+    useGlobalPrefix: true
+  });
 
   await app.listen(3000);
 }
