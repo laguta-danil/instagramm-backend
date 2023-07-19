f4fb3be79a79ec720e77a148a5bae37627701596
laguta.d.s@gmail.com <laguta.d.s@gmail.com>
2023-07-19 23:36:44 +0300

    swagger1010100

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { resolve } from 'path';
-import { writeFileSync } from 'fs';
-import * as process from 'process';
+import * as fs from 'fs';
+import * as path from 'path';
+const swagger = require('./swagger');
+import swaggerUI from 'swagger-ui-express';
 
 import { AppModule } from './app.module';
 
@@ -19,20 +20,63 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/swagger', app, document);
 
-  await app.listen(3000);
+  const options = {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui.css'
+  };
+  const spec = JSON.parse(
+    fs.readFileSync(path.join(__dirname, '../package.json'), 'utf8')
+  );
+  app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(spec, options));
 
   // get the swagger json file (if app is running in development mode)
-  if (process.env.NODE_ENV === 'development') {
-    const pathToSwaggerStaticFolder = resolve(process.cwd(), 'swagger-static');
+  // if (process.env.NODE_ENV === 'development') {
+  //   // write swagger ui files
+  //   https.get(
+  //     `${process.env.DEPLOYED_URL}/swagger/swagger-ui-bundle.js`,
+  //     function (response) {
+  //       response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
+  //       console.log(
+  //         `Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`
+  //       );
+  //     }
+  //   );
+  //
+  //   https.get(
+  //     `${process.env.DEPLOYED_URL}/swagger/swagger-ui-init.js`,
+  //     async function (response) {
+  //       response.pipe(
+  //         await createWriteStream('swagger-static/swagger-ui-init.js')
+  //       );
+  //       console.log(
+  //         `Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`
+  //       );
+  //     }
+  //   );
+  //
+  //   https.get(
+  //     `${process.env.DEPLOYED_URL}/swagger/swagger-ui-standalone-preset.js`,
+  //     function (response) {
+  //       response.pipe(
+  //         createWriteStream('swagger-static/swagger-ui-standalone-preset.js')
+  //       );
+  //       console.log(
+  //         `Swagger UI standalone preset file written to: '/swagger-static/swagger-ui-standalone-preset.js'`
+  //       );
+  //     }
+  //   );
+  //
+  //   https.get(
+  //     `${process.env.DEPLOYED_URL}/swagger/swagger-ui.css`,
+  //     function (response) {
+  //       response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
+  //       console.log(
+  //         `Swagger UI css file written to: '/swagger-static/swagger-ui.css'`
+  //       );
+  //     }
+  //   );
+  // }
 
-    // write swagger json file
-    const pathToSwaggerJson = resolve(
-      pathToSwaggerStaticFolder,
-      'swagger.json'
-    );
-    const swaggerJson = JSON.stringify(document, null, 2);
-    writeFileSync(pathToSwaggerJson, swaggerJson);
-    console.log(`Swagger JSON file written to: '/swagger-static/swagger.json'`);
-  }
+  await app.listen(3000);
 }
 bootstrap();
