4d7b06f3dce9578e1c4194297f3d22ccdf50bddd
Andrii Chertkov <andriichertkov@gmail.com>
2023-07-21 15:34:37 +0300

    add ExistUserByLoginOrEmail decorator

--- src/modules/user/repositories/user.repo.ts
+++ src/modules/user/repositories/user.repo.ts
@@ -5,4 +5,14 @@ import { PrismaService } from '../../../database/prisma.service';
 @Injectable()
 export class UsersRepo {
   constructor(private readonly prisma: PrismaService) {}
+
+  async checkUserByEmailOrLogin(emailOrLogin: string) {
+    const res = await this.prisma.user.findFirst({
+      where: { OR: [{ email: emailOrLogin }, { login: emailOrLogin }] }
+    });
+
+    console.log('REPO', res);
+
+    return res;
+  }
 }
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 
+import { UsersQueryRepo } from './repositories/user.query.repo';
+import { UsersRepo } from './repositories/user.repo';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [
+    // service
+    UserService,
+    UsersRepo,
+    UsersQueryRepo
+  ]
 })
 export class UserModule {}
