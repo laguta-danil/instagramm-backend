37cd9fb3a4f7d21a5b49b08f2696c9d2d8282e02
Andrii Chertkov <andriichertkov@gmail.com>
2023-07-21 16:27:32 +0300

    add sendRegistrationEmail

--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { EnvEnum } from '../../utils/env.enum';
+
+const { MAILER_SUBJECT } = EnvEnum;
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+  private readonly logger = new Logger(EmailService.name);
+
+  constructor(
+    private mailerService: MailerService,
+    private configService: ConfigService
+  ) {}
+
+  async sendRegistrationEmail(email: string, code: string) {
+    try {
+      await this.mailerService.sendMail({
+        context: {
+          code
+        },
+        subject: this.configService.get(MAILER_SUBJECT),
+        template: './register',
+        to: email
+      });
+
+      return true;
+    } catch (error) {
+      let message: string;
+      if (error instanceof Error) {
+        message = error.message;
+      }
+      this.logger.error(`Error sending email to ${email}: ${message}`);
+
+      return null;
+    }
+  }
+}
