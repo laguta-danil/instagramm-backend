f5e7386ddd6159b68283a47fba0c3ff8d5dbebc9
Andrii Chertkov <andriichertkov@gmail.com>
2023-07-21 18:07:31 +0300

    add stub for auth tests

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -1,3 +1,28 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule]
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
+  });
+});
+
 // import request from 'supertest';
 
 // import { HttpStatus } from '@nestjs/common';
