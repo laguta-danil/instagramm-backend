3d73e9bf0a58f4df09767ae0d52902812bb79b41
Andrii Chertkov <andriichertkov@gmail.com>
2023-07-22 13:37:46 +0300

    feat: resending dto

---
+++ src/modules/auth/dto/email.resending.dto.ts
@@ -0,0 +1,21 @@
+import { IsEmail, IsString, Matches, Validate } from 'class-validator';
+
+import { IsValidResendingCode } from '../../../infra/decorator/auth/is.valid.resending.code';
+import { Trim } from '../../../infra/decorator/validation/trim';
+
+export class EmailResendingDto {
+  @Validate(IsValidResendingCode)
+  @Matches(
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  )
+  @IsEmail()
+  @Trim()
+  @IsString()
+  readonly email: string;
+}
+
+export class EmailResendingDbDto {
+  readonly email: string;
+  readonly newCode: string;
+  readonly newExpDate: string;
+}
